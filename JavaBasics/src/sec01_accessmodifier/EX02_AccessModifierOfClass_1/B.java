9a63fd6281188ad67f679ed68fc26b58cb4324f2
ksks1234 <logging2021@gmail.com>
2025-06-12 18:13:54 +0900

    접근지정자-같은 패키지에서 클래스 접근 지정자와 생성자 접근 지정자 학습

---
+++ JavaBasics/src/sec01_accessmodifier/EX02_AccessModifierOfClass_1/B.java
@@ -0,0 +1,7 @@
+package sec01_accessmodifier.EX02_AccessModifierOfClass_1;
+
+// default 클래스
+class B {
+	// default 기본 생성자를 컴파일러가 추가
+
+}
---
+++ JavaBasics/src/sec01_accessmodifier/EX02_AccessModifierOfClass_1/C.java
@@ -0,0 +1,10 @@
+package sec01_accessmodifier.EX02_AccessModifierOfClass_1;
+
+// public 클래스
+public class C {
+	// default 기본 생성자 직접 생성
+	C() {
+		
+	}
+
+}
