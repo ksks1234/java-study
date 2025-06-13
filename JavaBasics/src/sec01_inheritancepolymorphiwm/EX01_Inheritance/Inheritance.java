62bac1f3056b4596e25da86a512463153e6614f3
ksks1234 <logging2021@gmail.com>
2025-06-13 10:40:23 +0900

    상속 - 클래스의 상속과 사용할 수 있는 멤버 학습

---
+++ JavaBasics/src/sec01_inheritancepolymorphiwm/EX01_Inheritance/Inheritance.java
@@ -0,0 +1,50 @@
+package sec01_inheritancepolymorphiwm.EX01_Inheritance;
+
+class Human{
+	String name;
+	int age;
+	void eat() {};
+	void sleep() {};
+}
+
+class Student extends Human {
+	int studentID;
+	void goToSchool() {};
+}
+
+class Worker extends Human {
+	int workerID;
+	void goToWork() {}
+}
+
+public class Inheritance {
+	public static void main(String[] args) {
+		// Human 객체 생성
+		Human h = new Human();
+		h.name = "김현지";
+		h.age = 11;
+		h.eat();
+		h.sleep();
+		
+		// Student 객체 생성
+		Student s = new Student();
+		s.name = "김민성";
+		s.age = 16;
+		s.studentID = 128;	// 추가로 정의한 멤버
+		s.eat();
+		s.sleep();
+		s.goToSchool(); 	// 추가로 정의한 멤버
+		
+		// Worker 객체 생성
+		Worker w = new Worker();
+		w.name = "봉윤정";
+		w.age = 45;
+		w.workerID = 128;	// 추가로 정의한 멤버
+		w.eat();
+		w.sleep();
+		w.goToWork();		// 추가로 정의한 멤버
+		
+	}
+	
+
+}
